289fc9ba6697a9f7c046d260e32252117e2ae490
Jacklyne Bouier <jacklynebouier@Jacklynes-MacBook-Pro.local>
2017-04-28 09:25:43 -0700

    twitter stream and post working

--- bot.js
+++ bot.js
@@ -6,17 +6,30 @@ var config = require('./config');
 
 var T = new Twit(config);
 
-var params = {
-  q: 'rainbow',
-  count: 2
-};
-
-T.get('search/tweets', params, gotData);
 
 
-function gotData(err, data, response) {
-  var tweets = data.statuses;
-  for (var i=0; i < tweets.length; i++) {
-    console.log(tweets[i].text);
+// var params = {
+//   q: 'Nintendo since:2017-04-27',
+//   count: 10
+// };
+//
+// T.get('search/tweets', params, gotData);
+//
+//
+// function gotData(err, data, response) {
+//   var tweets = data.statuses;
+//   for (var i=0; i < tweets.length; i++) {
+//     console.log(tweets[i].text);
+//   }
+// }
+
+var  options = {
+   screen_name: 'Qween_Wasabi',
+   count: 40
+              };
+
+T.get('statuses/user_timeline', options , function(err, data) {
+  for (var i = 0; i < data.length ; i++) {
+    console.log(data[i].text);
   }
-}
+});
